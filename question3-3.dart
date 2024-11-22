4a18461776b930948407cfe12aa84caac3b41cf9
พัฒนิสร ไกรสร <phattanisonk65@nu.ac.th>
2024-11-22 20:31:23 +0700

    question3

--- question3-3.dart
+++ question3-3.dart
@@ -1,8 +1,27 @@
-void createUser({required String name, required int age, bool isActive = true}) {
+import 'dart:io';
 
+void createUser({required String name, required int age, bool isActive = true}) {
   print("ชื่อ: $name อายุ: $age Active: $isActive ");
 }
 
 void main() {
-  createUser(name: "ธนากร ", age: 30);
+  stdout.write("กรุณากรอกชื่อ: ");
+  String? name = stdin.readLineSync();
+
+  stdout.write("กรุณากรอกอายุ: ");
+  String? ageInput = stdin.readLineSync();
+  int age = int.tryParse(ageInput ?? '') ?? 0;  
+
+
+  stdout.write("กรุณากรอกสถานะการใช้งาน : ");
+  String? isActiveInput = stdin.readLineSync();
+  bool isActive = isActiveInput?.toLowerCase() == 'false' ? false : true;
+
+  
+  if (name != null && name.isNotEmpty && age > 0) {
+   
+    createUser(name: name, age: age, isActive: isActive);
+  } else {
+    print("กรุณากรอกข้อมูลที่ถูกต้อง");
+  }
 }
